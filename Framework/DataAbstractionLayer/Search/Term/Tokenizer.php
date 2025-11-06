5c28e120700baac3ff278f968272684de0245a9c
Florian Müllenstädt <f.muellenstaedt>
2025-11-06 09:23:24 +0100

    fix that productnumbers with / inside can be used in search

--- Framework/DataAbstractionLayer/Search/Term/Tokenizer.php
+++ Framework/DataAbstractionLayer/Search/Term/Tokenizer.php
@@ -21,7 +21,6 @@ class Tokenizer implements TokenizerInterface
     public function tokenize(string $string): array
     {
         $string = mb_strtolower(html_entity_decode($string), 'UTF-8');
-        $string = trim(str_replace(['/', '\\'], ' ', $string));
         $string = str_replace('<', ' <', $string);
         $string = strip_tags($string);
 
